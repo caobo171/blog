a761253b8da4c9c5ec98d93ab17d3c759c43e5fd
Cao Nguyen Van <cao.nv17@gmail.com>
2021-12-25 23:44:04 +0700

    Support answer section

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,36 +4,64 @@ const bcrypt = require("bcrypt");
 
 //REGISTER
 router.post("/register", async (req, res) => {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPass = await bcrypt.hash(req.body.password, salt);
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPass,
-    });
+	try {
 
-    const user = await newUser.save();
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+		let exist_user = await User.findOne({ email: req.body.email });
+		if (exist_user) {
+			return res.status(200).json({
+				message: 'User email is existed'
+			});
+		}
+
+		exist_user = await User.findOne({ username: req.body.username });
+		if (exist_user) {
+			return res.status(200).json({
+				message: 'Username is existed'
+			});
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPass = await bcrypt.hash(req.body.password, salt);
+		const newUser = new User({
+			username: req.body.username,
+			email: req.body.email,
+			password: hashedPass,
+		});
+
+		const user = await newUser.save();
+		res.status(200).json(user);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 });
 
 //LOGIN
 router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong credentials!");
+	try {
+		let user = await User.findOne({ username: req.body.username });
 
-    const validated = await bcrypt.compare(req.body.password, user.password);
-    !validated && res.status(400).json("Wrong credentials!");
+		console.log('user', user);
 
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+		if (!user) {
+			user = await User.findOne({ email: req.body.username });
+		}
+
+		if (!user) {
+			return res.status(400).json("Wrong credentials!");
+		} 
+
+		const validated = await bcrypt.compare(req.body.password, user.password);
+		if (!validated) {
+			return res.status(400).json("Wrong credentials!");
+		}
+
+		const { password, ...others } = user._doc;
+		return res.status(200).json(others);
+	} catch (err) {
+		console.log('err', err);
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
